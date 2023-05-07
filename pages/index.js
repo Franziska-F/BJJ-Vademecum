b179ef641721f312dd52d1eeba00255e6eccecef
Franziska-F <f.fouchs@gmail.com>
2023-05-07 23:32:32 +0200

    Add styling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,21 +11,29 @@ export default function Home() {
         <div>
           <h1>BJJ</h1>
         </div>
-        <div className="flex flex-row">
-          <div className="w-1/2 m-4">
-            <img src="/img/technique_background.jpg" alt="two bjj fighters" />
+        <div className="flex flex-row justify-center rounded-md">
+          <div className="h-36 w-24 m-4">
+            <img
+              src="/img/technique_background.jpg"
+              className="h-36 w-24 rounded-md"
+              alt="two bjj fighters"
+            />
           </div>
-          <div className="bg-sky-950 w-1/2 m-4">Deine Techniken</div>
+          <div className="bg-sky-950 w-24 h-36 m-4 rounded-md text-white">
+            Deine Techniken
+          </div>
         </div>
         <div>
           <h2>Kategorien</h2>
-          <div className="grid grid-cols-4 gap-4">
-            <div className="bg-sky-950">Armbars</div>
-            <div className="bg-sky-950">Chokes</div>
-            <div className="bg-sky-950">Takedowns</div>
-            <div className="bg-sky-950">Sweeps</div>
-            <div className="bg-sky-950">Passes</div>
-            <div className="bg-sky-950">Legbars</div>
+          <div className="grid grid-cols-3 gap-4 p-8 text-white">
+            <Link className="bg-sky-950 w-18 h-12 rounded-md" href="">
+              Armbars
+            </Link>
+            <div className="bg-sky-950 w-18 h-12 rounded-md ">Chokes</div>
+            <div className="bg-sky-950 w-18 h-12 rounded-md">Takedowns</div>
+            <div className="bg-sky-950 w-18 h-12 rounded-md">Sweeps</div>
+            <div className="bg-sky-950 w-18 h-12 rounded-md">Passes</div>
+            <div className="bg-sky-950 w-18 h-12 rounded-md">Legbars</div>
           </div>
         </div>
       </main>
